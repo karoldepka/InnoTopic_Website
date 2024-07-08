4606d87e4661f97b6fdf0afd780a5acf5a5f99ff
Karol Depka <karol-depka@users.noreply.github.com>
2024-07-08 15:26:19 +0200

    window.localStorage.setItem('isPrint', true)

--- InnoTopicWebsite/src/app/TopicFriendsShared3/topics-core/print.service.ts
+++ InnoTopicWebsite/src/app/TopicFriendsShared3/topics-core/print.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class PrintService {
 
-  static isPrint = true // hack for now
+  static isPrint = !! JSON.parse(window.localStorage.getItem('isPrint') ?? 'false') // true // hack for now
 
   constructor() { }
 }
