86e4c2866a229906a981b68aa639c9ba768f9920
Karol Depka <karol-depka@users.noreply.github.com>
2024-07-26 00:29:45 +0200

    decl personal data

--- InnoTopicWebsite/src/app/cv-page/personal-data/personal-data.component.ts
+++ InnoTopicWebsite/src/app/cv-page/personal-data/personal-data.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface InfoItem {
+  label: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-personal-data',
   templateUrl: './personal-data.component.html',
   styleUrls: ['./personal-data.component.sass']
 })
 export class PersonalDataComponent implements OnInit {
+
+
+  infoItems: InfoItem[] = [
+    { label: 'First Name', value: 'Karol' },
+    { label: 'Last Name', value: 'Depka Pradzinski' },
+    { label: 'Birth Date', value: '1984-12-07' },
+    { label: 'Pronouns', value: 'he/him/himself <!--TODO abTesting() with Bro/Monster etc :D -->' },
+    { label: 'Nationality', value: 'Polish' },
+    { label: 'Education', value: 'Computer Science University Engineering Degree - Computer Programming and Networks (maximum grade) <!-- TODO sub-title note different color -->' },
+    { label: "Driver's license", value: 'B (Passenger cars)' },
+  ];
 
   constructor() { }
 
