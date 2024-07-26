27971a4880dbf24fdcfca67b93dc0a5ada9f5efa
Karol Depka <karol-depka@users.noreply.github.com>
2024-07-27 00:53:10 +0200

    ppl hashtags replacer

--- InnoTopicWebsite/src/app/topics-shared/hashtag-replacer/hashtag-replacer.component.ts
+++ InnoTopicWebsite/src/app/topics-shared/hashtag-replacer/hashtag-replacer.component.ts
@@ -14,6 +14,7 @@ import { Component, Input } from '@angular/core';
   `,
 })
 export class HashtagReplacerComponent {
+
   @Input() text: string = 'Test Hello #Angular and #Ionic™ ! ';
 
   get processedText(): { text: string; isTag: boolean; tagText?: string }[] {
