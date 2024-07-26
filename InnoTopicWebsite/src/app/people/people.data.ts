84ec3fd156d195d164c782d60c7fffb6e4be4fc6
Karol Depka <karol-depka@users.noreply.github.com>
2024-07-27 01:42:19 +0200

    ppl page updates WIP

--- InnoTopicWebsite/src/app/people/people.data.ts
+++ InnoTopicWebsite/src/app/people/people.data.ts
@@ -13,6 +13,25 @@ export const companies = {
   InnoTopic: "InnoTopic SLU"
 }
 
+export const timezones = {
+  CET: {
+    utcOffset: 2 // TODO summer time and whatnot; we can fetch from some DB, but overkill ahahaha
+  }
+}
+
+export const cities = {
+  Malaga: {
+    city: "Málaga",
+    countryCodeISO3166_1Alpha2: "ES",
+    coords: {
+      // TODO: lat lng @Jay
+    },
+    timeZone: timezones.CET,
+  }
+}
+
+// later this could come from db e.g. Supabase
+
 export const people = {
 
   karol: person({
@@ -21,8 +40,8 @@ export const people = {
     position: "CEO, Chief Software Developer",
     company: companies.InnoTopic,
     // note we r using "position", not "title", as title, could be smth like PhD.
-    tagline: "#Rust and #TypeScript enthusiast with a broad experience, especially in #Frontend"
-
+    tagline: "#Rust and #TypeScript enthusiast with a broad experience, especially in #Frontend",
+    location: cities.Malaga
   }),
 
 
