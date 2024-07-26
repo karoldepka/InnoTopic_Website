06efead0f4c5b76852bb16e71533b0854ee7a9da
Joisco <joisco@hotmail.com>
2024-07-26 16:22:53 -0700

    Updated people.data.ts

--- InnoTopicWebsite/src/app/people/people.data.ts
+++ InnoTopicWebsite/src/app/people/people.data.ts
@@ -35,7 +35,7 @@ export const people = {
 
   "Joisco": person({
     position: "#LLM intern specializing in #Python",
-    tagline: "Finding creative solutions with #LLM-s, #Python, and #LangChain"
+    tagline: "Finding creative solutions with #LLM-s, #Python, #AutoGen and #LangChain"
   }),
 
   jay: person({
