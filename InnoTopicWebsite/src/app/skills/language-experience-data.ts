f1a6f79b2cc17bfc5782656c48707a1a9757f9ea
Karol Depka <karol-depka@users.noreply.github.com>
2024-07-31 16:58:36 +0200

    fix: language icons: es-vc and au

--- InnoTopicWebsite/src/app/skills/language-experience-data.ts
+++ InnoTopicWebsite/src/app/skills/language-experience-data.ts
@@ -2,10 +2,10 @@ import { experience, Language, LanguageLevel } from "./language-experience";
 
 export const LanguageExperiences: Language[] = [
   new Language('Polish', experience('pl', LanguageLevel.NATIVE)),
-  new Language('English', experience('us gb in ca au', LanguageLevel.PROFESSIONAL)),
-  new Language('Spanish', experience('es mx co ar vz', LanguageLevel.PROFESSIONAL)),
+  new Language('English', experience('us gb in ca au' /* au */, LanguageLevel.PROFESSIONAL)),
+  new Language('Spanish', experience('es mx co ar' /* vz */, LanguageLevel.PROFESSIONAL)),
   new Language('German', experience('de at ch', LanguageLevel.PROFESSIONAL)),
-  new Language('Català', experience('es-ct es-vc', LanguageLevel.INTERMEDIATE)),
+  new Language('Català', experience('es-ct' /* es-vc */, LanguageLevel.INTERMEDIATE)),
   new Language('Portuguese', experience('pt br', LanguageLevel.INTERMEDIATE)),
   new Language('French', experience('fr be', LanguageLevel.BEGINNER)),
   new Language('Italian', experience('it', LanguageLevel.BEGINNER)),
