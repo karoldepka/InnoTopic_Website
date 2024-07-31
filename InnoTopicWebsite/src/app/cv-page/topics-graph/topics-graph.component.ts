4634c78c82a4aed6ae664eba89ebadb357972bdb
Jay Tailor <tailorj64@gmail.com>
2024-07-31 13:12:52 +0530

    fix: display topic graph container changes based on a toggle

--- InnoTopicWebsite/src/app/cv-page/topics-graph/topics-graph.component.ts
+++ InnoTopicWebsite/src/app/cv-page/topics-graph/topics-graph.component.ts
@@ -21,11 +21,6 @@ export class TopicsGraphComponent implements OnInit {
   private d3Links: LinkByIds[] = [...nodeLinks];
   graphHasContainer = false;
 
-  @Input()
-  config = {
-    containerMul: 0.2
-  }
-
   // idea: new/expanding-to topics could be with effect e.g. static noise or fading in-out, e.g. qwik, turbopack; while old, permanently faded
   // TODO: try d3.forceRadial(radius[, x][, y])
 
@@ -34,7 +29,7 @@ export class TopicsGraphComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.graphHasContainer = Boolean(this.activatedRoute.snapshot.queryParams['container']);
+    this.graphHasContainer = Boolean(this.activatedRoute.snapshot.queryParams['container'] === 'true');
     console.log('generateNodes', this.d3Nodes)
     this.generateNodes(this.connections)
     console.log('d3Nodes', this.d3Nodes)
@@ -91,8 +86,6 @@ export class TopicsGraphComponent implements OnInit {
 
   private initD3Graph() {
 
-    const config = this.config
-
     const self = this;
     const svgRootElement = d3.select("#topics-graph-d3"),
       width = +svgRootElement.attr("width"),
@@ -132,26 +125,48 @@ export class TopicsGraphComponent implements OnInit {
     const color = d3.color(`rgba(80, 80, 80, 0.5)`) // .copy({opacity: 0.5});
 
     /* Base Example: Force-Directed Graph: https://bl.ocks.org/mbostock/4062045 */
-    const simulation = d3.forceSimulation()
+    const simulation = d3.forceSimulation();
+      if(this.graphHasContainer) {
+        simulation.nodes(this.d3Nodes)
+          .force("link", d3.forceLink().id(function(d: any) { return d.id; })
+            .strength(function(d: any) {
+              return preset.forceLinkStrength * (d.strengthMul ?? 1)
+            })
+          )
+          .force("charge", d3.forceManyBody().strength(function(d: GraphNode) {
+            const size = d.sizeMult ?? midSize;
+            return size**1.5 * preset.forceManyBodyStrength / 1
+          }))
+          .force("center", d3.forceCenter(width / 2, height / 2))
+          .force("collide", d3.forceCollide().radius(function(d: any) {
+            return d.sizeMult ? d.sizeMult * 30 : 30; // Adjust the radius as needed
+          }));
+      } else {
       // .force("gravity", 3)
       // .velocityDecay(3)
-      .nodes(this.d3Nodes)
-    .force("link", d3.forceLink().id(function(d: any) { return d.id; })
-      .strength(function(d: any) {
-        return preset.forceLinkStrength * (d.strengthMul ?? 1)
-      }))
-    .force("charge", d3.forceManyBody().strength(function(d: GraphNode) {
-      const size = d.sizeMult ?? midSize;
-      return size**1.5 * preset.forceManyBodyStrength / 1
-    }))
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .force("collide", d3.forceCollide().radius(function(d: any) {
-      return d.sizeMult ? d.sizeMult * 30 : 30; // Adjust the radius as needed
-    }));
-    // simulation.force("charge", function() {
-    ////        return (d.sizeMult ? d.sizeMult : 1) * 100 }
-    //            return -1000000;
-    //        })
+      simulation.force("link", d3.forceLink().id(function(d: any) { return d.id; })
+        .strength(function(d: any) {
+          if (d.strengthMul) {
+            // console.log('d.strengthMul', d.strengthMul)
+          }
+          // return preset.forceLinkStrength;
+          // return 1 / Math.min(count(link.source), count(link.target));
+          return preset.forceLinkStrength * (d.strengthMul ?? 1)
+        }))
+        .force("charge", d3.forceManyBody().strength(function(d: GraphNode) {
+          const size = d.sizeMult ?? midSize;
+          // return preset.forceManyBodyStrength
+          // return size**5 * preset.forceManyBodyStrength / 3
+          // return size**10 * preset.forceManyBodyStrength / 100 // this was kinda working
+          return size ** 1.5 * preset.forceManyBodyStrength / 1 // this was kinda working
+          // return size * 1000000
+        }));
+        // simulation.force("charge", function() {
+        ////        return (d.sizeMult ? d.sizeMult : 1) * 100 }
+        //            return -1000000;
+        //        })
+      }
+
     const nodes = {};
     const nodesKeys = Object.keys(nodes);
     const nodesArray = nodesKeys.map(function(v) { return (nodes as any)[v]; });
@@ -312,16 +327,18 @@ export class TopicsGraphComponent implements OnInit {
       //   .attr("y1", function(d: any) { return d.source.y; })
       //   .attr("x2", function(d: any) { return d.target.x; })
       //   .attr("y2", function(d: any) { return d.target.y; });
-
-
-      perNodeMainGroup
-           .attr("cx", function(d: any) { return d.x = Math.max(radiusFunc(d), Math.min(width - radiusFunc(d), d.x)); })
-           .attr("cy", function(d: any) { return d.y = Math.max(radiusFunc(d), Math.min(height - radiusFunc(d), d.y)); });
-        
-
-       nodeCircleOverlay /* need to set position separately, because of issue with drag&drop and "translate(...)" transform */
-         .attr("x", function(d: any) { return (d.fx || d.x) - radiusFunc(d); })
-         .attr("y", function(d: any) { return (d.fy || d.y) - radiusFunc(d); });
+      if(self.graphHasContainer) {
+        perNodeMainGroup
+          .attr("cx", function(d: any) { return d.x = Math.max(radiusFunc(d), Math.min(width - radiusFunc(d), d.x)); })
+          .attr("cy", function(d: any) { return d.y = Math.max(radiusFunc(d), Math.min(height - radiusFunc(d), d.y)); });
+      } else {
+        perNodeMainGroup
+          .attr("x", function(d: any) { return (d.x - radiusFunc(d) ); })
+          .attr("y", function(d: any) { return (d.y - radiusFunc(d) ); });
+      }
+      nodeCircleOverlay /* need to set position separately, because of issue with drag&drop and "translate(...)" transform */
+        .attr("x", function(d: any) { return (d.fx || d.x) - radiusFunc(d); })
+        .attr("y", function(d: any) { return (d.fy || d.y) - radiusFunc(d); });
       perNodeMainGroup.attr("transform", function(d: any) {
         // return "translate(" + (d.x + radiusFunc(d) / 2) + "," + (d.y + radiusFunc(d) / 2) + ")";
         return "translate(" + (d.x) + "," + (d.y) + ")";
