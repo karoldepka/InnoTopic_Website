71829bf32f551f4c4fb3e1aa900ef2aeaf90d93f
Jay Tailor <tailorj64@gmail.com>
2024-08-11 18:11:09 +0530

    chore: add sub-module and display images

--- InnoTopicWebsite/src/app/shirt-generator/services/custom-prompt.service.ts
+++ InnoTopicWebsite/src/app/shirt-generator/services/custom-prompt.service.ts
@@ -18,8 +18,8 @@ type Response<T> = {
 })
 export class CustomPromptService extends AbstractTopicsPromptService {
   // TODO: 1. implement logic to return list of svg name from the response ✅
-  // TODO: 2. make a separate component that renders svg icon based on icon name from the response (create full path from public directory)
-  // TODO: 3. create git sub-module to have all svg icons (from repo https://github.com/gilbarbara/logos) in the public directory
+  // TODO: 2. make a separate component that renders svg icon based on icon name from the response (create full path from public directory) ✅
+  // TODO: 3. create git sub-module to have all svg icons (from repo https://github.com/gilbarbara/logos) in the public directory ✅
   // TODO: 4. optional - maybe need to create script that loads files to public directory
 
   constructor(
