5cd409ff6fb6471e1c62157d2f54470be0e38dde
Karol Depka <karol-depka@users.noreply.github.com>
2024-09-15 00:43:53 +0200

    router.include_router(qanda_routes.router)

--- ShirtGenV2/app/routes/routes.py
+++ ShirtGenV2/app/routes/routes.py
@@ -1,10 +1,12 @@
 # Consider separating route group initialization for better modularity
 from fastapi import APIRouter
 from app.routes import prompt_routes
+from app.routes import qanda_routes
 
 router = APIRouter(prefix="/ai-api")
 
 router.include_router(prompt_routes.router)
+router.include_router(qanda_routes.router)
 
 # If you add more routes, include them here in a modular way
 # router.include_router(another_routes.router)
