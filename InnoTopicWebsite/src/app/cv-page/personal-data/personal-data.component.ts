49ab167f5417990f719a1fbdb000a2c2bce93c73
Karol Depka <karol-depka@users.noreply.github.com>
2024-09-15 00:18:26 +0200

    add "his" pronoun

--- InnoTopicWebsite/src/app/cv-page/personal-data/personal-data.component.ts
+++ InnoTopicWebsite/src/app/cv-page/personal-data/personal-data.component.ts
@@ -17,7 +17,7 @@ export class PersonalDataComponent implements OnInit {
     { label: 'First Name', value: 'Karol' },
     { label: 'Last Name', value: 'Depka Pradzinski' },
     { label: 'Birth Date', value: '1984-12-07' },
-    { label: 'Pronouns', value: 'he/him/himself <!--TODO abTesting() with Bro/Monster etc :D -->' },
+    { label: 'Pronouns', value: 'he/him/himself/his <!--TODO abTesting() with Bro/Monster etc :D -->' },
     { label: 'Nationality', value: 'Polish' },
     { label: 'Education', value: 'Computer Science University Engineering Degree - Computer Programming and Networks (maximum grade) <!-- TODO sub-title note different color -->' },
     { label: "Driver's license", value: 'B (Passenger cars)' },
