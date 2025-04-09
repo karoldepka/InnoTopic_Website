c60d3205a9e37f253b28fc3ca8f68d1294bf904b
Karol <195425850+karolrvn2@users.noreply.github.com>
2025-04-09 22:12:25 +0200

    merch gen svg loading stub

--- InnoTopicWebsite/src/app/merch-gen/merch-gen.module.ts
+++ InnoTopicWebsite/src/app/merch-gen/merch-gen.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { MerchGenPageRoutingModule } from './merch-gen-routing.module';
+import {MerchGenPageRoutingModule} from './merch-gen-routing.module';
 
-import { MerchGenPage } from './merch-gen.page';
+import {MerchGenPage} from './merch-gen.page';
 
 @NgModule({
   imports: [
