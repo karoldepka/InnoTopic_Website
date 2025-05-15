8bee50d9e8a8101dac35f5722151ecd69dc19cec
Karol Depka <195425850+karolrvn2@users.noreply.github.com>
2025-05-15 22:28:18 +0200

    fix path mind/The_Spirit_Molecule.svg

--- InnoTopicWebsite/src/app/TopicFriendsShared3/topics-core/topics-data.ts
+++ InnoTopicWebsite/src/app/TopicFriendsShared3/topics-core/topics-data.ts
@@ -1075,7 +1075,7 @@ export class FunAndSports {
   'Ѱ' = tNarrow('generic/mind/Ѱ.svg') // & neuroscience
   'Ѱƛ⍹' = tWide('generic/mind/Ѱƛ⍹.svg')
   'ΜΔΜΑ' = tWide('generic/mind/ΜΔΜΑ.svg')
-  'The_Spirit_Molecule' = tWide('generic/mindThe_Spirit_Molecule.svg')
+  'The_Spirit_Molecule' = tWide('generic/mind/The_Spirit_Molecule.svg')
   'Guitar' = t('generic/fun/heavy-metal-sharpen-guitar-like-an-insect-svgrepo-com.svg')
   'ASG' = t('generic/fun/gun.svg')
 }
