34f8350db91c4c36b1b670b07f75ae16bb6bd923
Karol Depka Pradzinski The First <195425850+karoldepka@users.noreply.github.com>
2026-08-22 18:47:22 +0200

    Update build information

--- InnoTopicWebsite/lifesuite/src/environments/build-info.ts
+++ InnoTopicWebsite/lifesuite/src/environments/build-info.ts
@@ -2,6 +2,6 @@ export const buildInfo = {
   branch: "local",
   commit: "local",
   commitAuthor: "Karol Depka Pradzinski The First",
-  commitMessage: "Use solid Ionicons everywhere",
-  builtAt: "2026-08-22T16:13:17.172Z",
+  commitMessage: "Clarify unselected star rating state",
+  builtAt: "2026-08-22T16:44:00.394Z",
 };
